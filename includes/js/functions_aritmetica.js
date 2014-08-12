27763d5a75c17f460da11f3feabec3722b32d249
JMarqz <jmarqz04@gmail.com>
2014-08-12 16:18:55 -0500

    MathGenerator
    Sumas, restas, multiplicaciones y divisiones funcionando correctamente.
    Correcciones mínimas.

--- includes/js/functions_aritmetica.js
+++ includes/js/functions_aritmetica.js
@@ -37,6 +37,106 @@ function sumas(){
 }
 
 
+// RESTAS
+function restas(){
+	var nEjercicios = document.getElementById("number").value;
+	var nMax = document.getElementById("rango-max").value;
+	var nMin = document.getElementById("rango-min").value;
+	var respuestas = $("#respuestas").is(":checked");
+	var tipoResultados = $('input[name="radio-resultados"]:checked').val();
+
+
+	// Crear Array con números aleatorios
+	var matrizRestas = new Array();
+
+	for(var i=0; i<nEjercicios; i++){
+		matrizRestas.push(new Array());
+
+		for(var j=0; j<2; j++){
+			switch(tipoResultados){
+				case '1':
+					// Todos los resultados
+					var randomNum1 = aleatorio(nMax,nMin);
+					matrizRestas[i].push(randomNum1);
+				break;
+				case '2':
+					// Resultados positivos incluyendo 0
+					if (j==0) {
+						var randomNum1 = aleatorio(nMax,nMin);
+						matrizRestas[i].push(randomNum1);
+					} else{
+						randomNum1 = aleatorio(matrizRestas[i][0],nMin);
+						matrizRestas[i].push(randomNum1);
+					}
+				break;
+				case '3':
+					// Resultados positivos excluyendo 0
+					if (j==0) {
+						var randomNum1 = aleatorio(nMax,nMin);
+						matrizRestas[i].push(randomNum1);
+					} else{
+						if (matrizRestas[i][0]!=1) {
+							randomNum1 = aleatorio(matrizRestas[i][0]-1,nMin);
+							matrizRestas[i].push(randomNum1);
+						} else{
+							matrizRestas[i].push(0);
+						}					
+					}
+				break;
+				case '4':
+					// Solo resultados negativos
+					if (nMax==1) {
+						nMax = 2;
+					};
+					if (j==0) {
+						var randomNum1 = aleatorio(nMax-1,nMin);
+						matrizRestas[i].push(randomNum1);
+					} else{
+						randomNum1 = aleatorio(nMax,matrizRestas[i][0]);
+						if (randomNum1 > matrizRestas[i][0]) {
+							matrizRestas[i].push(randomNum1);
+						} else{
+							j--;
+						}
+					}
+				break;
+			}
+		}
+	}
+	
+	// Generar Restas
+	var problemas = "";
+	var resultadoEjercicio = 1;
+	
+	for (var i=0; i<matrizRestas.length; i++) {
+		for(var j=0; j<matrizRestas[i].length; j++){
+			var tmp = matrizRestas[i][j];
+			
+			if (j==0) {
+				resultadoEjercicio = tmp;
+			} else{
+				resultadoEjercicio -= tmp;
+			}
+
+			if (j<matrizRestas[i].length-1) {
+				// Sin respuestas
+				problemas += tmp + " - ";
+			} else{
+				if (respuestas) {
+					// Con respuestas
+					problemas += tmp + " (R: " + resultadoEjercicio + ")" + "\n";
+				} else{
+					problemas += tmp + "\n";
+				}
+			}
+		}
+		resultadoEjercicio = 1;
+	};
+	
+	document.getElementById("resultado").value = problemas;
+}
+
+
 // MULTIPLICACIONES
 function multiplicaciones(){
 	var nEjercicios = document.getElementById("number").value;
@@ -136,106 +236,6 @@ function divisiones(){
 			if (j<matrizDivisiones[i].length-1) {
 				// Sin respuestas
 				problemas += tmp + " / ";
-			} else{
-				if (respuestas) {
-					// Con respuestas
-					problemas += tmp + " (R: " + resultadoEjercicio + ")" + "\n";
-				} else{
-					problemas += tmp + "\n";
-				}
-			}
-		}
-		resultadoEjercicio = 1;
-	};
-	
-	document.getElementById("resultado").value = problemas;
-}
-
-
-// RESTAS
-function restas(){
-	var nEjercicios = document.getElementById("number").value;
-	var nMax = document.getElementById("rango-max").value;
-	var nMin = document.getElementById("rango-min").value;
-	var respuestas = $("#respuestas").is(":checked");
-	var tipoResultados = $('input[name="radio-resultados"]:checked').val();
-
-
-	// Crear Array con números aleatorios
-	var matrizDivisiones = new Array();
-
-	for(var i=0; i<nEjercicios; i++){
-		matrizDivisiones.push(new Array());
-
-		for(var j=0; j<2; j++){
-			switch(tipoResultados){
-				case '1':
-					// Todos los resultados
-					var randomNum1 = aleatorio(nMax,nMin);
-					matrizDivisiones[i].push(randomNum1);
-				break;
-				case '2':
-					// Resultados positivos incluyendo 0
-					if (j==0) {
-						var randomNum1 = aleatorio(nMax,nMin);
-						matrizDivisiones[i].push(randomNum1);
-					} else{
-						randomNum1 = aleatorio(matrizDivisiones[i][0],nMin);
-						matrizDivisiones[i].push(randomNum1);
-					}
-				break;
-				case '3':
-					// Resultados positivos excluyendo 0
-					if (j==0) {
-						var randomNum1 = aleatorio(nMax,nMin);
-						matrizDivisiones[i].push(randomNum1);
-					} else{
-						if (matrizDivisiones[i][0]!=1) {
-							randomNum1 = aleatorio(matrizDivisiones[i][0]-1,nMin);
-							matrizDivisiones[i].push(randomNum1);
-						} else{
-							matrizDivisiones[i].push(0);
-						}					
-					}
-				break;
-				case '4':
-					// Solo resultados negativos
-					if (nMax==1) {
-						nMax = 2;
-					};
-					if (j==0) {
-						var randomNum1 = aleatorio(nMax-1,nMin);
-						matrizDivisiones[i].push(randomNum1);
-					} else{
-						randomNum1 = aleatorio(nMax,matrizDivisiones[i][0]);
-						if (randomNum1 > matrizDivisiones[i][0]) {
-							matrizDivisiones[i].push(randomNum1);
-						} else{
-							j--;
-						}
-					}
-				break;
-			}
-		}
-	}
-	
-	// Generar Divisiones
-	var problemas = "";
-	var resultadoEjercicio = 1;
-	
-	for (var i=0; i<matrizDivisiones.length; i++) {
-		for(var j=0; j<matrizDivisiones[i].length; j++){
-			var tmp = matrizDivisiones[i][j];
-			
-			if (j==0) {
-				resultadoEjercicio = tmp;
-			} else{
-				resultadoEjercicio -= tmp;
-			}
-
-			if (j<matrizDivisiones[i].length-1) {
-				// Sin respuestas
-				problemas += tmp + " - ";
 			} else{
 				if (respuestas) {
 					// Con respuestas
